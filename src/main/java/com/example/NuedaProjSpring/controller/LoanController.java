9eff48a1bb166934bc142361c97b127a4a08c5b3
Riya Bemby <riyabemby@Riyas-MacBook-Pro.local>
2024-06-26 17:30:30 -0400

    added more data and additional endpoint for all loans

--- src/main/java/com/example/NuedaProjSpring/controller/LoanController.java
+++ src/main/java/com/example/NuedaProjSpring/controller/LoanController.java
@@ -1,6 +1,8 @@
 
 package com.example.NuedaProjSpring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,12 +10,17 @@ import com.example.NuedaProjSpring.model.Loan;
 import com.example.NuedaProjSpring.service.Payment;
 
 @RestController
-@RequestMapping("api/loans") // https:localhost:8080/api/accounts
+@RequestMapping("api/loans") // https:localhost:8080/api/loans
 @CrossOrigin
 public class LoanController {
     // handles REST API endpoints; Spring boot has web servers, as soon as we run it listens for HTTP connections
     @Autowired // grabs from factory and sticks it in here
     private Payment service;
+
+    @GetMapping() // declares that this method handles get request at endpoint (http://localhost:8080/api/loans)
+    public List<Loan> getAllLoans(){
+        return service.returnList();
+    }
 
     @GetMapping("/{loanID}") // declares that this method handles get request at endpoint (http://localhost:8080/api/loans/123456)
     public Loan getLoan(@PathVariable("loanID") String loanID){
--- src/main/java/com/example/NuedaProjSpring/service/Payment.java
+++ src/main/java/com/example/NuedaProjSpring/service/Payment.java
@@ -18,8 +18,17 @@ public class Payment {
 
     public Payment(){
         this.add(new StudentLoan("sg2309", 4.24, 100000, 10));
-        this.add(new Mortgage("rb17048", 7.90, 300000, 10));
-        this.add(new PersonalLoan("yb18044", 36, 10000, 3));
+        this.add(new StudentLoan("temp123", 4.24, 50000, 15));
+        this.add(new StudentLoan("ID345", 4.24, 10000, 5));
+
+        this.add(new Mortgage("rb1478", 7.90, 300000, 10));
+        this.add(new Mortgage("ab9102", 7.90, 250000, 15));
+        this.add(new Mortgage("xyz123", 7.90, 400000, 20));
+
+        this.add(new PersonalLoan("loanID", 36, 10000, 3));
+        this.add(new PersonalLoan("bm387", 36, 10000, 5));
+        this.add(new PersonalLoan("yb18044", 36, 5000, 3));
+
     }
 
     public void add(Loan x){
@@ -72,5 +81,13 @@ public class Payment {
             }
         }
         return null;
+    }
+
+    public List<Loan> returnList(){
+        List<Loan> where = new ArrayList<Loan>();
+        for (Loan x : loans){
+            where.add(x);
+        }
+        return where;
     }
 }
